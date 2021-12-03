3314ca73045b8f5b24505c2882ccc8ec16e9f473
Edgar Saul Junior Yalle Muñoz <85427794+SaulYm@users.noreply.github.com>
2021-12-03 18:57:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # Importar las librerias necesarias
 import streamlit as st
 import pandas as pd
-import tensorflow as tf
-from tensorflow import keras
+#import tensorflow as tf
+#from tensorflow import keras
 from streamlit.elements.arrow import Data
 from streamlit.elements.legacy_altair import generate_chart
+
+from tensorflow.keras.models import load_model
 
 #from tensorflow import keras
 #from tensorflow.keras.models import load_model
@@ -15,7 +17,7 @@ from streamlit.elements.legacy_altair import generate_chart
 model = ''
 #Cargar los datos
 if model == '':
-    model = tf.keras.models.load_model("ANN.h5")
+    model = load_model("ANN.h5")
     
 # Titulo
 st.title('JAVIER SE LA COME')
@@ -29,38 +31,33 @@ st.subheader('Artificial Neural Network')
 def user_input_parameters(): 
         
     # Variables
-    gender_dic = {'Male':0, 'Female':1, 'Other':2}
-    gender = st.sidebar.selectbox('Seleccione su género', gender_dic)
-    gender_num = gender_dic.get(gender)
+    options = ['No','Yes']
+
+
+
+    gender_opc = ['Male', 'Female', 'Other']
+    gender = st.sidebar.selectbox('Seleccione su género', gender_opc)
 
     age = st.sidebar.text_input('Ingrese su edad')
 
-    option_dic = {'No':0, 'Yes':1}
+    heart_disease = st.sidebar.radio('¿Sufre de problemas al corazón?', options)
 
-    heart_disease = st.sidebar.radio('¿Sufre de problemas al corazón?', option_dic)
-    heart_disease_num = option_dic.get(heart_disease)
+    hypertension = st.sidebar.radio('¿Sufre de hipertensión?', options)
 
-    hypertension = st.sidebar.radio('¿Sufre de hipertensión?', option_dic)
-    hypertension_num = option_dic.get(hypertension)
+    ever_married = st.sidebar.radio('¿Alguna vez se casó?', options)
 
-    ever_married = st.sidebar.radio('¿Alguna vez se casó?', option_dic)
-    ever_married_num = option_dic.get(ever_married)
+    work_type_opc = ['children','Govt_job','Never_worked','Private','Self-employed']
+    work_type = st.sidebar.selectbox('Seleccione su tipo de trabajo', work_type_opc)
 
-    work_type_dic = {'children':0,'Govt_job':1,'Never_worked':2,'Private':3,'Self-employed':4}
-    work_type = st.sidebar.selectbox('Seleccione su tipo de trabajo', work_type_dic)
-    work_type_num = work_type_dic.get(work_type)
-
-    residence_type_dic = {'Rural':0,'Urban':1}
-    residence_type = st.sidebar.radio('¿En qué tipo de residencia vive?', residence_type_dic)
-    residence_type_num = residence_type_dic.get(residence_type)
+    residence_type_opc = ['Rural','Urban']
+    residence_type = st.sidebar.radio('¿En qué tipo de residencia vive?', residence_type_opc)
                 
     avg_glucose_level = st.sidebar.text_input('Ingrese su nivel de glucosa')
 
     bmi = st.sidebar.text_input('Índice de masa corporal')
 
-    smoking_status_dic = {"formerly smoked":0, "never smoked":1, "smokes":2, "Unknown":3}
-    smoking_status = st.sidebar.selectbox('Tipo de fumador', smoking_status_dic)
-    smoking_status_num = smoking_status_dic.get(smoking_status)
+    smoking_status_opc = ["formerly smoked", "never smoked", "smokes", "Unknown"]
+    smoking_status = st.sidebar.selectbox('Tipo de fumador', smoking_status_opc)
 
     data_query = {
                     'gender': gender,
@@ -81,6 +78,9 @@ query_df = user_input_parameters()
 
 st.write(query_df)
 
+query_df = query_df.replace({"Male":0, "Female":1, "Other":2, "No":0, "Yes":1, "children":0,"Govt_job":1,"Never_worked":2,"Private":3,"Self-employed":4,
+                         "Rural":0,"Urban":1,"formerly smoked":0, "never smoked":1, "smokes":2, "Unknown":3})
+#Falta arreglar esto ;v
 if st.button('RUN'):
     predict = model.predict(query_df)
     if predict == 1:
@@ -91,9 +91,3 @@ if st.button('RUN'):
 
 #if __name__ == '__main__':
     #main()
-
-    
-
-
-
-
